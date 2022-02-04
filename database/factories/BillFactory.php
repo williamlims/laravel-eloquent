c55040d4d3256fbbd09c7701d76ca85d374de107
William Lima <williamlims@hotmail.com>
2022-02-03 23:15:20 -0300

    Atualização dos módulos

--- database/factories/BillFactory.php
+++ database/factories/BillFactory.php
@@ -14,7 +14,12 @@ class BillFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'invoice' =>$this->faker->randomNumber($nbDigits = 4, $strict = false),
+            'installment' => $this->faker->randomDigit,
+            'client_id' => $this->faker->uuid, 
+            'value' => $this->faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 99),
+            'due_date' => $this->faker->dateTimeBetween($startDate = 'now', $endDate = '+1 week', $timezone = null),
+            'payment_date' => $this->faker->dateTimeBetween($startDate = '-1 week', $endDate = 'now', $timezone = null)
         ];
     }
 }
